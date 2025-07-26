84cc5717da28a1fdb7735b9f5c465528b9d08d80
RamyaSree Chebrolu <2300030576@kluniversity.in>
2025-07-26 16:38:37 +0530

    Updated homepage and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/S219_CICD/'
-});
+  base: '/S219_CICD/', // 👈 Important for GitHub Pages!
+  plugins: [react()]
+})
